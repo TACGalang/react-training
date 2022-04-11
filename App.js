4d7d2789b300f117a76d4e5c5f51f8f5f834d4ba
Tristan Angelo C. Galang <tac.galang@gmail.com>
2022-04-12 00:45:00 +0800

    First functional react native app

--- App.js
+++ App.js
@@ -1,11 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Button } from "react-native";
+import InputView from "./components/InputView";
+import React, { useState } from "react";
+import TrainingItem from "./components/TrainingItem";
 
 export default function App() {
+  const [trainingLogs, setTrainingLogs] = useState([]);
+  const [isAddModalVisible, setAddModalVisibility] = useState(false);
+
+  const onAddTraining = (trainingLog) => {
+    let logModel = { uID: Math.random().toString(), value: trainingLog };
+    setTrainingLogs((currentLogs) => currentLogs.concat(logModel));
+    setAddModalVisibility(false);
+  };
+
+  const onRemove = (uID) => {
+    setTrainingLogs((currentLogs) => {
+      return currentLogs.filter((log) => log.uID !== uID);
+    });
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FlatList
+        style={styles.flatList}
+        keyExtractor={(item, index) => item.uID}
+        data={trainingLogs}
+        renderItem={(itemData) => (
+          <TrainingItem
+            uID={itemData.item.uID}
+            title={itemData.item.value}
+            onSelect={onRemove}
+          />
+        )}
+      />
+
+      <Button
+        title="Add Training"
+        onPress={() => setAddModalVisibility(true)}
+      />
+
+      {/* modal */}
+      <InputView
+        isVisible={isAddModalVisible}
+        onAddLogs={onAddTraining}
+        onCancel={() => setAddModalVisibility(false)}
+      />
     </View>
   );
 }
@@ -13,8 +52,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "column",
+    justifyContent: "space-between",
+    paddingTop: 50,
+    padding: 20,
+  },
+
+  flatList: {
+    flex: 1,
   },
 });
